060bea5f2ce1b83b17cd7346b4ad1be2431ddc34
Zin Myo Thant <zinmyothant.dev@gmail.com>
2024-05-06 22:45:51 +0630

    Complete DreamDictionary

---
+++ ZMTDotNetCore.RestApiWithNLayer/Feature/DreamDictionary/DreamDictionaryController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace ZMTDotNetCore.RestApiWithNLayer.Feature.DreamDictionary
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DreamDictionaryController : ControllerBase
+    {
+       public DreamDictionaryController() { }
+        private  async Task<DreamDictionaryModel> Data()
+        {
+            string data =await System.IO.File.ReadAllTextAsync("DreamDictionary.json");
+            var json=JsonConvert.DeserializeObject<DreamDictionaryModel>(data);
+            return json;
+        }
+
+        [HttpGet("GetAllDictionary")]
+        public async Task<IActionResult> GetAllDictionary()
+        {
+            var model = await Data();
+            return Ok(model.BlogDetail);
+        }
+
+        [HttpGet("GetAlphabet")]
+        public async Task<IActionResult> GetAlphabet()
+        {
+            var model = await Data();
+            return Ok(model.BlogHeader);
+        }
+
+        [HttpGet("{BlogId}")]
+        public async Task<IActionResult> GetDictionaryByAlphabet(int BlogId)
+        {
+            var model = await Data();
+            return Ok(model.BlogDetail.Where(x => x.BlogId == BlogId));
+        }
+    }
+}
---
+++ ZMTDotNetCore.RestApiWithNLayer/Feature/DreamDictionary/DreamDictionaryModel.cs
@@ -0,0 +1,23 @@
+﻿namespace ZMTDotNetCore.RestApiWithNLayer.Feature.DreamDictionary
+{
+
+    public class DreamDictionaryModel
+    {
+        public Blogheader[] BlogHeader { get; set; }
+        public Blogdetail[] BlogDetail { get; set; }
+    }
+    
+    public class Blogheader
+    {
+        public int BlogId { get; set; }
+        public string BlogTitle { get; set; }
+    }
+
+    public class Blogdetail
+    {
+        public int BlogDetailId { get; set; }
+        public int BlogId { get; set; }
+        public string BlogContent { get; set; }
+    }
+
+}
