50601860103868600a1f44881bd3fec0008f18f0
DESKTOP-SSEBD4Q\KZH <zinmyothant@acedatasystems.com>
2024-05-28 15:08:48 +0630

    Blog List Frm

---
+++ ZMTDotNetCore.WinFormsApp/FrmBlogList.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace ZMTDotNetCore.WinFormsApp
+{
+    partial class FrmBlogList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dgv = new DataGridView();
+            colId = new DataGridViewTextBoxColumn();
+            colTitle = new DataGridViewTextBoxColumn();
+            colAuthor = new DataGridViewTextBoxColumn();
+            colContent = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)dgv).BeginInit();
+            SuspendLayout();
+            // 
+            // dgv
+            // 
+            dgv.AllowUserToAddRows = false;
+            dgv.AllowUserToDeleteRows = false;
+            dgv.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgv.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgv.Columns.AddRange(new DataGridViewColumn[] { colId, colTitle, colAuthor, colContent });
+            dgv.Location = new Point(0, 0);
+            dgv.Name = "dgv";
+            dgv.ReadOnly = true;
+            dgv.RowHeadersWidth = 51;
+            dgv.RowTemplate.Height = 29;
+            dgv.Size = new Size(800, 451);
+            dgv.TabIndex = 0;
+            // 
+            // colId
+            // 
+            colId.DataPropertyName = "BlogId";
+            colId.HeaderText = "Id";
+            colId.MinimumWidth = 6;
+            colId.Name = "colId";
+            colId.ReadOnly = true;
+            colId.Visible = false;
+            // 
+            // colTitle
+            // 
+            colTitle.DataPropertyName = "BlogTitle";
+            colTitle.HeaderText = "Title";
+            colTitle.MinimumWidth = 6;
+            colTitle.Name = "colTitle";
+            colTitle.ReadOnly = true;
+            // 
+            // colAuthor
+            // 
+            colAuthor.DataPropertyName = "BlogAuthor";
+            colAuthor.HeaderText = "Author";
+            colAuthor.MinimumWidth = 6;
+            colAuthor.Name = "colAuthor";
+            colAuthor.ReadOnly = true;
+            // 
+            // colContent
+            // 
+            colContent.DataPropertyName = "BlogContent";
+            colContent.HeaderText = "Content";
+            colContent.MinimumWidth = 6;
+            colContent.Name = "colContent";
+            colContent.ReadOnly = true;
+            // 
+            // FrmBlogList
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dgv);
+            Name = "FrmBlogList";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Blog List";
+            Load += FrmBlogList_Load;
+            ((System.ComponentModel.ISupportInitialize)dgv).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dgv;
+        private DataGridViewTextBoxColumn colId;
+        private DataGridViewTextBoxColumn colTitle;
+        private DataGridViewTextBoxColumn colAuthor;
+        private DataGridViewTextBoxColumn colContent;
+    }
+}
---
+++ ZMTDotNetCore.WinFormsApp/FrmBlogList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZMTDotNetCore.Shared;
+using ZMTDotNetCore.WinFormsApp.Model;
+
+namespace ZMTDotNetCore.WinFormsApp
+{
+    public partial class FrmBlogList : Form
+    {
+        private readonly DrapperService _drapperService;
+
+        public FrmBlogList()
+        {
+            InitializeComponent();
+            dgv.AutoGenerateColumns = false;
+            _drapperService = new DrapperService(ConnectionStrings.connectionStrings.ConnectionString);
+
+        }
+
+        private void FrmBlogList_Load(object sender, EventArgs e)
+        {
+           List<BlogModel> lst= _drapperService.Query<BlogModel>("select * from Tbl_Blog;");
+            dgv.DataSource = lst;
+        }
+    }
+}
