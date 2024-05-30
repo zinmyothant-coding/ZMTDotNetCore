f1d3690005920c970ae5414f235190fbe47a445e
DESKTOP-SSEBD4Q\KZH <zinmyothant@acedatasystems.com>
2024-05-30 14:51:46 +0630

    NLayer

---
+++ ZMTDotNetCore.NLayer.BusinessLogic/BL_Blog.cs
@@ -0,0 +1,45 @@
+﻿ 
+using ZMTDotNetCore.NLayer.DataAccess; 
+namespace ZMTDotNetCore.RestApiWithNLayer.Feature.Blog
+{
+    public class BL_Blog
+    {
+        private readonly DA_Blog _blog;
+
+        public BL_Blog()
+        {
+            _blog = new DA_Blog();
+        }
+        public List<BlogModel> GetBlogs()
+        {
+            var lst = _blog.GetBlogs();
+            return lst;
+        }
+        public BlogModel GetBlog(int id)
+        {
+            return _blog.GetBlog(id);
+        }
+        public int CreateBlog(BlogModel blog)
+        {
+           int result=_blog.CreateBlog(blog);
+            return result;
+        }
+        public int UpdateBlog(int id, BlogModel blog)
+        {
+            var item = _blog.GetBlog(id);
+            if (item is null) return 0;
+
+            
+            var result = _blog.UpdateBlog(id, blog);
+            return result;
+        }
+        public int DeleteBlog(int id)
+        {
+            var item = _blog.GetBlog(id);
+            if (item is null) return 0;
+            
+            var result = _blog.DeleteBlog(id);
+            return result;
+        }
+    }
+}
---
+++ ZMTDotNetCore.NLayer.UI/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ZMTDotNetCore.NLayer.DataAccess;
+using ZMTDotNetCore.RestApiWithNLayer.Feature.Blog;
+
+Console.WriteLine("Hello, World!");
+
+BL_Blog blog = new BL_Blog();
+BlogModel model = new BlogModel
+{
+    BlogAuthor="test0",
+    BlogContent="test",
+    BlogTitle="test"
+};
+blog.CreateBlog(model);
+Console.ReadKey();
